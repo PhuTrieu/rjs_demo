19f929355fc5c4f68eeb97e2431e2b5b388d6505
PhuTrieu <uyphu4@gmail.com>
2019-12-26 16:16:52 +0700

    Updates

---
+++ precache-manifest.b0e3bdf4690e04a314559e96dffc0195.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d95af7ff4c9f2980e822c614fff0a267",
+    "url": "/rjs_demo/index.html"
+  },
+  {
+    "revision": "40faaab8c1d14d310f7d",
+    "url": "/rjs_demo/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0d2fdfee3ae27d72ec1e",
+    "url": "/rjs_demo/static/js/2.e183147b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/rjs_demo/static/js/2.e183147b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "40faaab8c1d14d310f7d",
+    "url": "/rjs_demo/static/js/main.6f99f527.chunk.js"
+  },
+  {
+    "revision": "25e786cfbe20bea5929c",
+    "url": "/rjs_demo/static/js/runtime-main.b53b5782.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rjs_demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
